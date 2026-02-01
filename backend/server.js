ebf651d4d6bc9171e99a9464011bc412f40ea251
Peterson Vieira <petersonvieira2412@gmail.com>
2026-02-01 14:33:35 -0300

    fix: update server port from 5000 to 5010

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5010;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
